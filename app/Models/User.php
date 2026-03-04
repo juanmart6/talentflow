0c3c37a05de7fd78ed780a5f7447b75a94ded0d8
Juanma <jumago92@gmail.com>
2026-03-04 12:27:44 +0100

    Actualización: Configuración .env + Mailpit + Tareas Fase 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -35,6 +36,13 @@ class User extends Authenticatable
         'two_factor_recovery_codes',
         'remember_token',
     ];
+
+    /**
+     * Guard name for Spatie roles/permissions.
+     *
+     * @var string
+     */
+    protected $guard_name = 'web';
 
     /**
      * Get the attributes that should be cast.
